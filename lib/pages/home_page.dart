f6cb56772e1ddf041446bd2364e594556e5ccae8
kshitij patidar <kshitijpatidar2003@gmail.com>
2024-09-18 02:00:01 +0530

    mobile view changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/nav_items.dart';
+import 'package:my_portfolio/styles/style.dart';
+import 'package:my_portfolio/widgets/site_logo.dart';
+
+import '../widgets/header_desktop.dart';
+import '../widgets/header_mobile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,49 +23,9 @@ class _HomePageState extends State<HomePage> {
         scrollDirection: Axis.vertical,
         children: [
           //Main
-          Container(
-            height: 60,
-            margin: EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 20.0,
-            ),
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.transparent,
-                    CustomColor.bgLight1,
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(100)),
-            child: Row(
-              children: [
-                Text(
-                  "KP",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: CustomColor.yellowSecondary,
-                  ),
-                ),
-                Spacer(),
-                for (int i = 0; i < navTitles.length; i++)
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        navTitles[i],
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                          color: CustomColor.whitePrimary,
-                        ),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
+          //HeaderDesktop(),
+          HeaderMobile(
+            onMenuTap: () {},
           ),
           //SKILLS
           Container(
---
+++ lib/widgets/site_logo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class SiteLogo extends StatelessWidget {
+  const SiteLogo({super.key,this.onTap,});
+  final VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+                  onTap: onTap,
+                  child: const Text(
+                    "KP",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: CustomColor.yellowSecondary,
+                    ),
+                  ),
+                );
+  }
+}
