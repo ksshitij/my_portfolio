8bb91ff6b451a0c286b60e3ae0e1daf61b726879
kshitij patidar <kshitijpatidar2003@gmail.com>
2024-09-19 01:04:30 +0530

    made sure that mobile code work for mobile and laptop for laptop used layout builder to do so

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:my_portfolio/constants/colors.dart';
 import 'package:my_portfolio/constants/nav_items.dart';
+import 'package:my_portfolio/constants/size.dart';
 import 'package:my_portfolio/styles/style.dart';
 import 'package:my_portfolio/widgets/drawer_mobile.dart';
 import 'package:my_portfolio/widgets/site_logo.dart';
@@ -19,45 +21,51 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: const DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          //Main
-          //HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: () {},
-            onMenuTap: () {
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
-          //SKILLS
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          //PROJECTS
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-          //CONTACTS
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          //FOOTER
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-        ],
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        key: scaffoldKey,
+        backgroundColor: CustomColor.scaffoldBg,
+        endDrawer: constraints.maxWidth >= kMinDesktopWidth
+            ? null
+            : const DrawerMobile(),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            //Main
+            if (constraints.maxWidth >= kMinDesktopWidth)
+              const HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+            //SKILLS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            //PROJECTS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+            //CONTACTS
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.blueGrey,
+            ),
+            //FOOTER
+            Container(
+              height: 500,
+              width: double.maxFinite,
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
