9af7b30f9a4e610b10a1e93fdfbfa43d882ca4bf
kshitij patidar <kshitijpatidar2003@gmail.com>
2024-12-04 22:51:45 +0530

    added new res

--- lib/constants/sns_links.dart
+++ lib/constants/sns_links.dart
@@ -4,6 +4,6 @@ class SnsLinks {
       "https://www.linkedin.com/in/kshitij-patidar-969a8322b/";
   static const String instagram = "https://www.instagram.com/_kshitiijjj/";
   static const String resume =
-      "https://drive.google.com/file/d/1QDA7t0lvfWG0PRSma71lgo7HmDANtDq5/view?usp=sharing";
+      "https://drive.google.com/file/d/1ibXgLTmMX8c0ROyJYfldzffz5hOUc8g2/view?usp=sharing";
   static const String leetcode = "https://leetcode.com/u/kshitijpatidar/";
 }
